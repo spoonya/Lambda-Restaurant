badbbd1f604a8f3f6eea528c53c3c6a6b86537c1
spoonya <55800883+spoonya@users.noreply.github.com>
2019-10-23 22:39:49 +0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,36 @@
 let navToggle = $("#navToggle");
 let nav = $("#nav");
+let header = $("#header");
+let intro = $("#intro");
+let introH = intro.innerHeight();
+let scrollPos = $(this).scrollTop();
+
+function checkScroll (scrollPos, introH) {
+  if (scrollPos > introH) {
+    header.addClass("fixed");
+  } else {
+    header.removeClass("fixed");
+  }
+}
+
+checkScroll(scrollPos, introH);
+
+$(window).on("scroll resize", function () {
+  introH = intro.innerHeight();
+  scrollPos = $(this).scrollTop();
+  checkScroll(scrollPos, introH);
+});
+
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+  let elId = $(this).data("scroll");
+  let elOffset = $(elId).offset().top;
+  nav.removeClass("show");
+
+  $("html, body").animate( {
+      scrollTop: elOffset + 1
+    }, 500)
+})
 
 /*Burger*/
 navToggle.on("click", function (event) {
